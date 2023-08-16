7c1fc0d9c88eace9e2005e8a4cef795f0fdf2f56
Aon Mao <aonmao888@gmail.com>
2023-08-16 23:44:31 +0630

    10

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ let pass = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI
 let supabase = createClient(url,pass);
 app.set('views',__dirname+'/views');
 app.set('view engine','ejs');
+app.use(express.urlencoded({extended:true}))
 
 //get css file
 app.get('/home.css',(req,res)=>{res.sendFile(__dirname+'/views/css/home.css')})
@@ -21,70 +22,145 @@ app.get('/',(req,res)=>{
 })
 
 //get all students page
-app.get('/students',async(req,res)=>{
-    let {data,error} = await supabase.from('members').select().eq('type','student');
-    res.render('s',{all:data,page:'Students'})
+app.get('/students/:year',async(req,res)=>{
+    if(req.params.year == 2023){
+        let {data,error} = await supabase.from('2023').select().eq('type','student');
+        res.render('s',{all:data,page:'Students',year:2023})
+    }else{
+        res.send("No page found!")
+    }
 })
 
 //get teachers page
-app.get('/teachers',async(req,res)=>{
-    let {data,error} = await supabase.from('members').select().eq('type','teacher');
-    res.render('s',{all:data,page:'Teachers'})
+app.get('/teachers/:year',async(req,res)=>{
+    if(req.params.year == 2023){
+        let {data,error} = await supabase.from('2023').select().eq('type','teacher');
+        res.render('s',{all:data,page:'Teachers',year:2023})
+    }
 })
 
 //get pre GED page
-app.get('/pre-ged',async(req,res)=>{
-    let {data,error} = await supabase.from('members').select().eq('class','pre ged');
-    res.render('s',{all:data,page:'pre ged'})
+app.get('/pre-ged/:year',async(req,res)=>{
+    if(req.params.year == 2023){
+        let {data,error} = await supabase.from('2023').select().eq('class','pre ged');
+        res.render('s',{all:data,page:'pre ged',year:2023})
+    }
 })
 
 //get GED page
-app.get('/ged',async(req,res)=>{
-    let {data,error} = await supabase.from('members').select().eq('class','ged');
-    res.render('s',{all:data,page:'ged'})
+app.get('/ged/:year',async(req,res)=>{
+    if(req.params.year == 2023){
+        let {data,error} = await supabase.from('2023').select().eq('class','ged');
+        res.render('s',{all:data,page:'ged',year:2023})
+    }
 })
 
 //get ICT page
-app.get('/ict',async(req,res)=>{
-    let {data,error} = await supabase.from('members').select().eq('class','ict');
-    res.render('s',{all:data,page:'ict'})
+app.get('/ict/:year',async(req,res)=>{
+    if(req.params.year == 2023){
+        let {data,error} = await supabase.from('2023').select().eq('class','ict');
+        res.render('s',{all:data,page:'ict',year:2023})
+    }
 })
 
-app.get('/d',(req,res)=>{
-    let datab = onValue(ref(db, '/users/'), (snapshot) => {
-        const username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
-        // ...
-      }, {
-        onlyOnce: true
-      });
+//get Secondary page
+app.get('/secondary/:year',async(req,res)=>{
+    if(req.params.year == 2023){
+        let {data,error} = await supabase.from('2023').select().eq('class','secondary');
+        res.render('s',{all:data,page:'secondary',year:2023})
+    }
 })
 
 //get profile page
 app.get('/p/:id',async(req,res)=>{
     let id = req.params.id;
-    let {data,error} = await supabase.from('members').select().eq('member_id',id);
-    res.render('p',{all:data})
+    let {data,error} = await supabase.from('2023').select().eq('member_id',id);
+    res.render('p',{all:data,year:2023})
 })
 
 //get editor page
-app.get('/edit/:subject',async(req,res)=>{
+app.get('/edit/:class/:subject/:year', async(req,res)=>{
+    let clas = req.params.class;
+    let year = req.params.year;
     let subject = req.params.subject;
-    let {data,error} = await supabase.from('members').select().eq('class',subject);
-    let check = supabase.auth.onAuthStateChange((event,session)=>{
-        if(event === 'SIGNED_IN'){
-            console.log('user signed in:',session.user)
-        }else if(event === 'SIGNED_OUT'){console.log('User signed out')}
-    })
-    console.log(check)
-    if(data.length !== 0){
+    //console.log(clas+','+year+','+subject);
+    let {data,error} = await supabase.from(year).select().eq('class',clas);
+    if(data){
         res.render('e',{
             all:data,
-            title:subject
+            year:year,
+            clas:clas,
+            subject:subject
         })
-    }else{
-        res.send('no page found')
     }
-    
+})
+
+//listen login page
+app.get('/login',(req,res)=>{
+    res.render('login',{back:req.query.back})
+})
+
+//listen teacher login
+app.post('/login',async(req,res)=>{
+    const {email,pass} = req.body;
+    let {user,error}=await supabase.auth.signInWithPassword({
+        email:email,
+        password:pass
+    })
+    console.log(user)
+    if(error){
+        res.status(401).send('Login Failed')
+    }else{
+        res.redirect(`${req.query.back}`)
+    }
+    //console.log(email+','+pass)
+})
+
+//listen all students page
+app.get('/students',(req,res)=>{
+    res.render('allstudent')
+})
+
+//listen update student points
+app.post('/update/:year/:subject/:id',async(req,res)=>{
+    let sid = req.params.id;
+    let year = req.params.year;
+    let subject = req.params.subject;
+    let {point} = req.body;
+    let back = req.query.back;
+    let toback = back.split('/ed')[1]
+    console.log(sid+','+point+','+back+','+toback+','+year);
+    if(subject == 'tai'){
+        const {data,error} = await supabase.from(`${year}`).update({'tai' : point}).eq('member_id',`${sid}`);
+        res.redirect('/ed'+toback);
+    }else if(subject == 'burmese'){
+        const {data,error} = await supabase.from(`${year}`).update({'burmese' : point}).eq('member_id',`${sid}`);
+        res.redirect('/ed'+toback);
+    }else if(subject == 'english'){
+        const {data,error} = await supabase.from(`${year}`).update({'english' : point}).eq('member_id',`${sid}`);
+        res.redirect('/ed'+toback);
+    }else if(subject == 'mathematic'){
+        const {data,error} = await supabase.from(`${year}`).update({'mathematic' : point}).eq('member_id',`${sid}`);
+        res.redirect('/ed'+toback);
+    }else if(subject == 'science'){
+        const {data,error} = await supabase.from(`${year}`).update({'science' : point}).eq('member_id',`${sid}`);
+        res.redirect('/ed'+toback);
+    }else if(subject == 'thai'){
+        const {data,error} = await supabase.from(`${year}`).update({'thai' : point}).eq('member_id',`${sid}`);
+        res.redirect('/ed'+toback);
+    }else if(subject == 'chinese'){
+        const {data,error} = await supabase.from(`${year}`).update({'chinese' : point}).eq('member_id',`${sid}`);
+        res.redirect('/ed'+toback);
+    }else if(subject == 'art'){
+        const {data,error} = await supabase.from(`${year}`).update({'art' : point}).eq('member_id',`${sid}`);
+        res.redirect('/ed'+toback);
+    }else if(subject == 'ict_basic'){
+        const {data,error} = await supabase.from(`${year}`).update({'ict_basic' : point}).eq('member_id',`${sid}`);
+        res.redirect('/ed'+toback);
+    }else if(subject == 'ict_program'){
+        const {data,error} = await supabase.from(`${year}`).update({'ict_program' : point}).eq('member_id',`${sid}`);
+        res.redirect('/ed'+toback);
+    }
 })
 
 app.listen(80,()=>{console.log("server started with port 80")})
